7485edd9652142f0135ef13cce78410de3e61ad9
Juergen Adler <juergen.adler@gmx.net>
2024-02-26 01:21:55 +0100

    file find modified, still to do

--- js/index.js
+++ js/index.js
@@ -1,16 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let includes = document.getElementsByTagName('include');
-    for (var i = 0; i < includes.length; i++) 
-    {
-        let include = includes[i];
-        load_file(includes[i].attributes.src.value, function (text) 
-        {
-            include.insertAdjacentHTML('afterend', text);
-            include.remove();
-        });
-    }
-    function load_file(filename, callback) 
-    {
-        fetch(filename).then(response => response.text()).then(text => callback(text));
-    }
-});
+function addHTML() {
+   /*Iterate all DOM*/
+   let el = document.getElementsByTagName("div"); // We allow this attribute only in div tags
+   for (let i = 0; i < el.length; i++) {
+      let domEl = el[i];
+
+      /*find the element having include-html attribute*/
+      let fileName = domEl.getAttribute("include-html");
+      if (fileName) {
+         /*http request with attribute value as file name*/
+         let xmlHttp = new XMLHttpRequest();
+         xmlHttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+               if (this.status == 200) {
+                  domEl.innerHTML = this.responseText;
+               }
+               if (this.status == 404) {
+                  domEl.innerHTML = "Page not found.";
+               }
+
+               /* Remove the attribute and invoke the function again*/
+               domEl.removeAttribute("include-html");
+               addHTML();
+            }
+         }
+         xmlHttp.open("GET", fileName, true);
+         xmlHttp.send();      
+         return;
+      }
+   }
+}
+addHTML();
